34d806cc11ae1da1c3654eb99d0168618cf79358
Ryota Katoh <l94q.developer@gmail.com>
2016-01-24 00:36:27 +0900

    initial commit

---
+++ config.fish
@@ -0,0 +1,69 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH /Users/RyotaKatoh/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+set -gx OMF_CONFIG /Users/RyotaKatoh/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+
+# locale
+set -gx LC_ALL ja_JP.UTF-8
+set -gx LANG en_US.UTF-8
+
+# ls colors
+set -Ux LSCOLORS cxfxcxdxbxegedabagacad 
+
+# python
+alias ip="~/.pyenv/versions/2.7.10/bin/frameworkpython -m IPython"
+
+# golang
+set -x GOPATH ~/golang
+set PATH $GOPATH/bin $PATH
+
+# CFLAGS
+set -gx CFLAGS -I(xcrun --show-sdk-path)/usr/include
+
+# Spark
+set -gx IPYTHON 1
+alias spark "~/spark-1.5.2-bin-hadoop2.6/bin/pyspark"
+
+# Xcode
+alias xcode="open -a Xcode"
+
+# AWS EB
+if test -f ~/.AWSKEY;
+	cat ~/.AWSKEY | while read line;
+		eval (echo $line)
+	end
+end
+
+
+# NVM
+set -gx NVM_DIR (brew --prefix nvm)
+
+# git shortcuts
+function gs
+	git status
+end
+
+function ga
+	git add $argv
+end
+
+function gc
+	git commit $argv
+end
+
+function gco
+	git checkout $argv 
+end
+
+function gb
+	git branch $argv
+end
+
+# utilities
+function tz
+	totsuzen $argv
+end
---
+++ dotfilesLink.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# !!!attention!!!
+# this script remove following files!
+if test -f ~/.config/fish/config.fish;
+	rm ~/.config/fish/config.fish
+end
+if test -f  ~/.tmux.conf;
+	rm ~/.tmux.conf
+end
+
+if test -f ~/.tmux.conf;
+	rm ~/.tmux.conf
+end
+
+if test -f ~/.vimrc;
+	rm ~/.vimrc
+end
+
+ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vim ~/.vim/
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
